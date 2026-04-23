5edbed8d9e23ce5c929d8532c5f5696fea592f4d
Praze-hub <godspraiseokechukwu07@gmail.com>
2026-04-23 17:47:04 +0100

    Modified the PasswordReset flow

--- app/accounts/serializers.py
+++ app/accounts/serializers.py
@@ -8,6 +8,7 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_str, force_bytes
 from django.core.mail import send_mail
+from django.contrib.auth.tokens import default_token_generator
 
 
 CustomUser = get_user_model()
@@ -94,7 +95,8 @@ class PasswordResetRequestSerializer(serializers.Serializer):
         email = self.validated_data['email']
         user = CustomUser.objects.get(email=email)
         uid = urlsafe_base64_encode(force_bytes(user.pk))
-        token = PasswordResetTokenGenerator().make_token(user)
+        # token = PasswordResetTokenGenerator().make_token(user)
+        token = default_token_generator.make_token(user)
         
         if request:
             current_site = get_current_site(request).domain
@@ -119,7 +121,10 @@ class PasswordResetConfirmSerializer(serializers.Serializer):
         except Exception:
             raise serializers.ValidationError({"uid": "Invalid uid"})
         
-        if not PasswordResetTokenGenerator().check_token(self.user, attrs['token']):
+        # if not PasswordResetTokenGenerator().check_token(self.user, attrs['token']):
+        #     raise serializers.ValidationError({"token": "Invalid or expired token"})
+        
+        if not default_token_generator.check_token(self.user, attrs['token']):
             raise serializers.ValidationError({"token": "Invalid or expired token"})
         
         return attrs
